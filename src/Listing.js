0e77754babdcd7893b43a5935e566c3c4a6688fd
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-03-12 03:38:06 +0700

    dbjason

--- src/Listing.js
+++ src/Listing.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export const Listing = () => {
   return (
-    <div>Listing</div>
-  )
-}
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Listing</h2>
+        </div>
+        <div className="card-body">
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
